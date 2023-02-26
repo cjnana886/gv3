39afc3d243d00996215d513f6eeb6d3b07fffe66
ShengCJ <garrix@GarrixdeMacBook-Pro.local>
2023-02-26 16:06:27 +0800

    新增共用CSS和調整EP的CSS檔案位置

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        charset: false,
+        additionalData: '@use "@/assets/scss/_prepend.scss" as *;',
+      },
+    },
+  },
 });
