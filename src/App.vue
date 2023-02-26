65573210af0218f8658aa2e3b2b1608c76df31d7
ShengCJ <garrix@GarrixdeMBP.home>
2023-02-27 00:24:13 +0800

    新增幽靈場地和調整載入頁籤樣式

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,11 @@
-<script setup lang="ts"></script>
-
 <template lang="pug">
-header
-  img(
-    alt="Vue logo"
-    class="logo"
-    src="@/assets/logo.svg"
-    width="125"
-    height="125"
-  )
-
-  //- 標題
-  .title Welcome to vue3 world!!
-
-//- Route
-router-view
+.main-layout
+  //- Route
+  router-view
 </template>
 
 <style lang="scss" scoped>
-header {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  .title {
-    color: #01b468;
-  }
+.main-layout {
+  padding: 15px 20px;
 }
 </style>
